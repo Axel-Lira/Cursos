b5bd3333db8f137f51e362157a1fc624e2f9e441
Axel Lira <ricardo.axel.diaz.lira@gmail.com>
2020-12-21 13:41:44 -0600

    Curso React Terminado

--- Practicas React/src/components/App.js
+++ Practicas React/src/components/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import Exercises from '../pages/exercises'
-import ExerciseNew from '../pages/exercisesNew'
-import NotFound from '../pages/notfound'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ExercisesContainer from '../pages/ExercisesContainer'
+import ExerciseNewContainer from '../pages/ExerciseNewContainer'
+import NotFound from '../pages/404'
 
-const App=()=>(
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/exercise" component={Exercises} />
-                <Route exact path="/exercise/new" component={ExerciseNew} />
-                <Route component={NotFound} />
-            </Switch>
-        </BrowserRouter>
+const App = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/exercise" component={ExercisesContainer} />
+            <Route exact path="/exercise/new" component={ExerciseNewContainer} />
+            <Route component={NotFound}/>
+        </Switch>
+    </BrowserRouter>
 )
+
 export default App
--- Practicas React/src/index.js
+++ Practicas React/src/index.js
@@ -1,30 +1,8 @@
-/*const element =document.createElement('h1');
-element.innerText='Hello React';
-const container = document.getElementById('root');
-container.appendChild(element);*/
-
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App'
 
-/*function formatName(persona){
-  return persona.nombre + ' ' +persona.apellido;
-}
+const container = document.getElementById('root')
 
-function getGreeting(user){
-  if(user){
-    return <h1>Hola, {formatName(user)}</h1>;
-  }
-  return <h1>Hola, extraño</h1>
-}
-const persona = {
-  nombre:'Haxmell',
-  apellido:'Corp',
-  avatar: 'https://diaryofacrasian.files.wordpress.com/2012/08/gackt-malice-mizer.jpg'
-};
-const element = (<div>{getGreeting(persona)}<img src={persona.avatar} alt="Avatar" /></div>);
-const container = document.getElementById('root');
-ReactDOM.render(element,container);//que va a renderisar y donde lo hará*/
-const container = document.getElementById('root');
-ReactDOM.render(<App />,container);
+ReactDOM.render(<App />, container)
